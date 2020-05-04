33bfd4b2b8c3b1c79cfc6b26f9d7064b953fd690
Eli Filiba <efiliba@seven.com.au>
2020-05-04 10:00:49 +1000

    Display for JsonCellColumn placeholder

--- src/cell/json_cell_column.rs
+++ src/cell/json_cell_column.rs
@@ -1,3 +1,4 @@
+// use std::fmt::{self, Display};
 use crate::cell::SYMBOLS;
 
 #[derive(Debug)]
@@ -6,6 +7,12 @@ pub struct JsonCellColumn {
 	strike_out: bool,
 	highlight: bool
 }
+
+// impl Display for JsonCellColumn {
+//   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//     write!(f, "{{'{}': {}}}", "symbol", self.symbol)
+//   }
+// }
 
 impl JsonCellColumn {
   pub fn new(index: usize) -> JsonCellColumn {
